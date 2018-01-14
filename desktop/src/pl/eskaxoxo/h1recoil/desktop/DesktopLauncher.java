4866a2c61779f60785c74bee2b74ba6b5a1a17fb
eska <esakyu@gmail.com>
2018-01-14 22:45:57 +0100

    Added executables

--- desktop/src/pl/eskaxoxo/h1recoil/desktop/DesktopLauncher.java
+++ desktop/src/pl/eskaxoxo/h1recoil/desktop/DesktopLauncher.java
@@ -8,6 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		
+		config.title = "H1 Recoil Tester";
 		config.width = 800;
 		config.height = 450;
 		
